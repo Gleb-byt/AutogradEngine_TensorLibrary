100010f6fc824a3535e78d247c855332a4e81142
Gleb-byt <glebovgleb0205@gmail.com>
2026-09-06 23:21:05 +0300

    nothin special appeared, just tried to implement some variants of models for mnist dataset

--- python/nn.py
+++ python/nn.py
@@ -8,16 +8,30 @@
 # python folder
 
 
-import sys
+# import sys
 
-print(sys.version)
+# print(sys.version)
+
+# import build.bindings.autograd_engine as ae
+
+# shape = [2, 2]
+# data = list(map(float, range(1, 5)))
+
+# t = ae.Tensor(data=data, shape=shape)
+
+# print(data)
+# t.print()
+
+import typing as tp
 
 import build.bindings.autograd_engine as ae
 
-shape = [2, 2]
-data = list(map(float, range(1, 5)))
 
-t = ae.Tensor(data=data, shape=shape)
+class model:
+    def __init__(self, input_dimensions: int, output_dimensions: int, seed: int = 42):
+        self.module1 = ae.Linear(input_dimensions, 512)
+        self.module2 = ae.Linear(512, 256)
+        self.module3 = ae.linear(256, 64)
+        self.module4 = ae.Linear(16, output_dimensions)
 
-print(data)
-t.print()
+    def make_pred(input_data: ae.Tensor): ...
